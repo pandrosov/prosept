3de4c1b1050114379b3f136ece03a3ffca6f3539
pandrosov <pandrosov@list.ru>
2020-08-14 07:58:00 +0300

    heroku deploy

--- backend/server.js
+++ backend/server.js
@@ -14,4 +14,4 @@ app.get('*', (req, res) => {
 });
 
 
-app.listen(process.env.PORT || 80, () => {console.log("Server startea at port: 5000")})
+app.listen(process.env.PORT || 3000, () => {console.log("Server startea at port: 5000")})
